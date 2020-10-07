f22e6b18c6f6f06d17cd24efb2a2d551ca7bbc98
mohammad-miftakhus-sholikin <mohammadmiftakhussholikin@gmail.com>
2020-10-08 06:11:37 +0700

    2020-10-08 06:11:37

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Pelatihan Meta-Analisis Menggunakan R
 
-# A. Instalasi R
+# A. Instalasi r
 1. Download di [**sini**](https://cran.r-project.org/bin/windows/base/R-4.0.2-win.exe) atau pergi kelaman [**ini**](https://cran.r-project.org/bin/windows/base/). (**NB: file rada gede gan 80an MB**)
 2. Lakukan instalasi pada umumnya (pergi ke folder download -> klik dua kali file instaler -> ikuti petunjuk).
 
@@ -40,4 +40,6 @@
    Error in install.packages :
    ```
 
-# C. 
+# C. Download file pelatiahannya
+1. Klik di [sini](https://codeload.github.com/mohammad-miftakhus-sholikin/pelatihan-2020-metal/zip/master).
+2. Kemudian ekstrak file "pelatihan-2020-metal.zip".
--- Rscript_2020__Contoh _kuantitatif-2.R
+++ Rscript_2020__Contoh _kuantitatif-2.R
@@ -1,10 +1,11 @@
 #!/usr/bin/Rscript
 
 
-#Script	: Script_2020_Latihan
+#Script	: Rscript_2020__Contoh _kuantitatif-1
 #Author	: Mohammad Miftakhus Sholikin
-#Descr.	: Effect of Antimicrobial Peptide on Broiler Performance: Linear and Linear Mixed Model Evaluation
-#Date   	: 12-Mei-2020
+#Descr.	: Effect of Antimicrobial Peptide on Broiler Performance: Linear Model and Linear Mixed Model Evaluation
+#Date   	: 08-Oktober-2020
+
 
   
 
@@ -22,7 +23,8 @@ library(lme4)
 
 # input data
 #offline >> local directory
-#metal_data <- read_excel("Data_2020__AMP_broiler.xlsx")
+metal_data <- read_excel("Data_2020__AMP_broiler.xlsx")
+
 #online >> github
 get_url <- getURL("https://raw.githubusercontent.com/mohammad-miftakhus-sholikin/pelatihan-2020-metal/master/Data_2020__AMP_broiler.csv")
 metal_data <- read.csv(text=get_url, sep = ",", dec = ".")
