9227338e56e5c8d783d2b9c97e5451e5dbbe48f8
Mohammad Miftakhus Sholikin <mohammadmiftakhussholikin@gmail.com>
2021-08-14 14:27:59 +0700

    Update Rscript_2020__Contoh _kuantitatif-1.R

--- ch2_metal_mixed_model/Rscript_2020__Contoh _kuantitatif-1.R
+++ ch2_metal_mixed_model/Rscript_2020__Contoh _kuantitatif-1.R
@@ -1,32 +1,30 @@
 #!/usr/bin/Rscript
 
-
 #Script	: Rscript_2020__Contoh _kuantitatif-1
 #Author	: Mohammad Miftakhus Sholikin
 #Descr.	: Effect of Antimicrobial Peptide on Broiler Performance: Linear Mixed Model Evaluation
 #Date   	: 08-Oktober-2020
-
   
 
-# install new packages
-wants <- c("RCurl", "readxl", "lme4")
+# initialize
+## install new packages
+wants <- c("curl", "readxl", "lme4")
 has <- wants %in% rownames(installed.packages())
 if(any(!has)) install.packages(wants[!has])
 
-
-# input library
+## input library
 library(curl)
 library(readxl)
 library(lme4)
 
-
-# initialize
 ## check current directory
 getwd()
 
 ## set working directory
 #setwd("c:/Users/windows_name/Downloads/pelatihan-2020-metal-master") #check your path especially for this user windows_name
 
+
+# input and build model
 ## input data
 ### offline >> local directory
 metal_data <- read_excel("Data_2020__AMP_broiler.xlsx")
