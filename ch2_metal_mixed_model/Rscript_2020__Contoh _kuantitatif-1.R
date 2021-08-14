6f12c2bdd0aa74de4fd3f4ae05df2e4cada4fac6
Mohammad Miftakhus Sholikin <mohammadmiftakhussholikin@gmail.com>
2021-08-14 14:17:53 +0700

    Update Rscript_2020__Contoh _kuantitatif-1.R

--- ch2_metal_mixed_model/Rscript_2020__Contoh _kuantitatif-1.R
+++ ch2_metal_mixed_model/Rscript_2020__Contoh _kuantitatif-1.R
@@ -14,35 +14,31 @@ has <- wants %in% rownames(installed.packages())
 if(any(!has)) install.packages(wants[!has])
 
 
-
 # input library
 library(RCurl)
 library(readxl)
 library(lme4)
 
 
-
-# check current directory
+# initialize
+## check current directory
 getwd()
+
+## set working directory
 #setwd("c:/Users/windows_name/Downloads/pelatihan-2020-metal-master") #check your path especially for this user windows_name
 
-
-
-# input data
-#offline >> local directory
+## input data
+### offline >> local directory
 metal_data <- read_excel("Data_2020__AMP_broiler.xlsx")
-
-#online >> github
+### online >> github
 get_url <- getURL("https://raw.githubusercontent.com/mohammad-miftakhus-sholikin/pelatihan-2020-metal/master/Data_2020__AMP_broiler.csv")
 metal_data <- read.csv(text=get_url, sep = ",", dec = ".")
 
-
-# linear and linear mixed model
+## linear and linear mixed model
 lm_1 <- lm(bw ~ scale(level), data=metal_data)
 lm_2 <- lmer(bw ~ scale(level) + (1|study), data=metal_data)
 
 
-
-# BIC
+# BIC and AIC value
 BIC(lm_1, lm_2)
 AIC(lm_1, lm_2)
