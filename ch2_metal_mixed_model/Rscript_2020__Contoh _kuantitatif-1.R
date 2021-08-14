fe472ae322220a38d6ed14f796cb9f4b81467fc4
Mohammad Miftakhus Sholikin <mohammadmiftakhussholikin@gmail.com>
2021-08-14 14:21:38 +0700

    Update Rscript_2020__Contoh _kuantitatif-1.R

--- ch2_metal_mixed_model/Rscript_2020__Contoh _kuantitatif-1.R
+++ ch2_metal_mixed_model/Rscript_2020__Contoh _kuantitatif-1.R
@@ -15,7 +15,7 @@ if(any(!has)) install.packages(wants[!has])
 
 
 # input library
-library(RCurl)
+library(curl)
 library(readxl)
 library(lme4)
 
@@ -31,14 +31,25 @@ getwd()
 ### offline >> local directory
 metal_data <- read_excel("Data_2020__AMP_broiler.xlsx")
 ### online >> github
-get_url <- getURL("https://raw.githubusercontent.com/mohammad-miftakhus-sholikin/pelatihan-2020-metal/master/Data_2020__AMP_broiler.csv")
-metal_data <- read.csv(text=get_url, sep = ",", dec = ".")
+get_url <- curl("https://raw.githubusercontent.com/mohammad-miftakhus-sholikin/pelatihan-2020-metal/master/Data_2020__AMP_broiler.csv")
+metal_data <- read.csv(get_url, sep = ",", dec = ".")
 
-## linear and linear mixed model
+## model
+### linear and linear mixed model
 lm_1 <- lm(bw ~ scale(level), data=metal_data)
 lm_2 <- lmer(bw ~ scale(level) + (1|study), data=metal_data)
 
+## linear mixed model: ord1 and ord2
+lm_ord1 <- lmer(bw ~ scale(level) + (1|study), data=metal_data)
+lm_ord2 <- lmer(bw ~ scale(level) + I((scale(level))^2) + (1|study), data=metal_data)
 
-# BIC and AIC value
+## validation
+### BIC and AIC value
 BIC(lm_1, lm_2)
 AIC(lm_1, lm_2)
+
+## summary of the model
+summary(lm_lm1)
+summary(lm_lm2)
+summary(lm_ord1)
+summary(lm_ord2)
