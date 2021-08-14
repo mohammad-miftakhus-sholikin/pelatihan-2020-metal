a0ca8c104aa46bb9dee1b6e11494cbfe5e20c8f8
Mohammad Miftakhus Sholikin <mohammadmiftakhussholikin@gmail.com>
2021-08-14 14:27:53 +0700

    Update Rscript_2020__Contoh _kuantitatif-2.R

--- ch2_metal_mixed_model/Rscript_2020__Contoh _kuantitatif-2.R
+++ ch2_metal_mixed_model/Rscript_2020__Contoh _kuantitatif-2.R
@@ -1,38 +1,39 @@
 #!/usr/bin/Rscript
 
-
 #Script	: Rscript_2020__Contoh _kuantitatif-1
 #Author	: Mohammad Miftakhus Sholikin
 #Descr.	: Effect of Antimicrobial Peptide on Broiler Performance: Linear Model and Linear Mixed Model Evaluation
 #Date   	: 08-Oktober-2020
-
-
   
 
-#check library
-if(!require('pacman'))install.packages('pacman')
-pacman::p_load(RCurl,lme4)
+# initialize
+## install new packages
+wants <- c("curl", "readxl", "lme4")
+has <- wants %in% rownames(installed.packages())
+if(any(!has)) install.packages(wants[!has])
 
-# input library
+## input library
 library(RCurl)
 library(readxl)
 library(lme4)
-#library(nlme)
 
-
-# check current directory
+## check current directory
 getwd()
+
+## set working directory
 #setwd("c:/Users/windows_name/Downloads/pelatihan-2020-metal-master") #check your path especially for this user windows_name
 
-# input data
-#offline >> local directory
-metal_data <- read_excel("Data_2020__AMP_broiler.xlsx")
 
-#online >> github
+# input and build model
+## input data
+### offline >> local directory
+metal_data <- read_excel("Data_2020__AMP_broiler.xlsx")
+### online >> github
 get_url <- getURL("https://raw.githubusercontent.com/mohammad-miftakhus-sholikin/pelatihan-2020-metal/master/Data_2020__AMP_broiler.csv")
 metal_data <- read.csv(text=get_url, sep = ",", dec = ".")
 
-# linear and linear mixed model
+## model
+### linear and linear mixed model
 lm_1 <- lm(bw ~ scale(level), data=metal_data)
 lm_2 <- lm(bw ~ scale(level) + peptide, data=metal_data)
 lm_3 <- lm(bw ~ scale(level) + peptide + treatment, data=metal_data)
@@ -44,7 +45,7 @@ lm_8 <- lmer(bw ~ scale(level) + (1|peptide) + (1|treatment) + (1|broiler), data
 lm_9 <- lmer(bw ~ scale(level) + (1|peptide) + (1|treatment) + (1|broiler) + (1|study), data=metal_data)
 lm_10 <- lmer(bw ~ scale(level) + (1|peptide) + (1|treatment) + (1|broiler) + (1|study) + (1|broiler:treatment:study), data=metal_data)
 
-
-
-# BIC
+## validation
+### BIC and AIC value
 BIC(lm_1, lm_2, lm_3, lm_4, lm_5, lm_6, lm_7, lm_8, lm_9, lm_10)
+AIC(lm_1, lm_2, lm_3, lm_4, lm_5, lm_6, lm_7, lm_8, lm_9, lm_10)
